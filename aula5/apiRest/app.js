ddf43a5cec8faad8da27a0dcab32443a51381878
Willians Martins <professorwillians@gmail.com>
2021-03-19 20:27:08 -0300

    meio da aula

---
+++ aula5/apiRest/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var load = require('express-load');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+var mongoose = require('mongoose');
+global.db = mongoose.connect('mongodb://localhost:27017/contatos');
+
+load('models').into(app);
+
+// var Contato = app.models.contato;
+
+app.listen('3000', function(){
+  console.info("Servidor no ar!");
+})
+
+// app.get('/', function(request, response){
+//   response.send('Servidor no ar');
+// })
