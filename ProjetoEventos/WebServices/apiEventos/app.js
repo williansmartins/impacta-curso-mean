c21aad4e157df74627f64ec1f02e956ea1a8db8e
Willians Martins <professorwillians@gmail.com>
2021-03-23 23:00:47 -0300

    fim do modulo do angularJS

--- ProjetoEventos/WebServices/apiEventos/app.js
+++ ProjetoEventos/WebServices/apiEventos/app.js
@@ -113,7 +113,7 @@ app.delete('/eventos/:id', function (request, response) {
           response.json(erro);
         }
         else {
-          response.send('removido');
+          response.send('removido'); 
         }
       });
     }
