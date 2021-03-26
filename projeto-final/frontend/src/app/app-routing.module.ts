838a0d1cb2b7ec28de62855e903d64398187ee03
Willians Martins <professorwillians@gmail.com>
2021-03-26 19:45:14 -0300

    add estrutura do menu

--- projeto-final/frontend/src/app/app-routing.module.ts
+++ projeto-final/frontend/src/app/app-routing.module.ts
@@ -4,9 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './heroes/heroes.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  // { path: '', component: AppComponent },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
